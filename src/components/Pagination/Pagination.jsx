52797a5892a7f14c58616b97caccdc7d73595d65
Victor Pinheiro <contato.vspinheiro@gmail.com>
2025-06-12 12:58:16 -0300

    feat(paginação): criar componente de paginação com lógica e estilo responsivo
    - Adicionado botões de navegação de páginas (anterior/próxima)
    - Implementada lógica de navegação entre páginas
    - Estilização responsiva para dispositivos menores

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,54 @@
+import Button from '../Button/Button';
+
+import {UlStyle} from './Pagination.style'
+
+export default function Pagination({ total, totalPerPage, currentPage, setCurrentPage }) {
+  const totalPages = Math.ceil(total / totalPerPage);
+
+ 
+
+const handleClickPrevius = (currentPage) => {
+  if (currentPage > 0) {
+    return setCurrentPage(currentPage -1)
+  }
+
+
+}
+const handleClickNext = (currentPage, totalPages) => {
+  if (currentPage < totalPages -1) {
+    return setCurrentPage(currentPage +1)
+  }
+}
+const handleClick = (index) => {
+  return setCurrentPage(index );
+ }
+  return (
+    <nav>
+
+      <UlStyle>
+      <Button
+      children='Anterior'
+      onClick={() => handleClickPrevius(currentPage)}
+      />
+        {Array.from({length: totalPages}).map((_, index) => {
+          return (
+            <li key={index}>
+            
+            <Button
+            active={currentPage === index }
+            children={index +1}
+            onClick={() => handleClick(index)}
+            />
+            </li>
+          )
+        })}
+        <Button
+        children='Próxima'
+        active
+        onClick={() => handleClickNext(currentPage, totalPages) }
+
+        />
+      </UlStyle>
+    </nav>
+  )
+}
---
+++ src/components/Pagination/Pagination.style.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const UlStyle = styled.ul `
+  display: flex;
+  list-style: none;
+  gap: 10px; 
+  box-sizing: border-box;
+
+button {
+  height: 35px;
+  width: 100%;
+  padding: 10px;
+}
+
+  
+ @media (max-width: 600px)  {
+
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  padding: 10px;
+
+  button {
+    padding: 2.5px 5px;
+    height: 25px;
+    font-size: 12px;
+  
+  }
+  
+ 
+}
+`
